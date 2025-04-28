5cfc0195ef3aec97fa4e7068bbf1cde03e4bdb6e
Subhajitnaskar99 <subhajitnaskar99@gmail.com>
2025-04-28 21:57:32 +0530

    Module 3 task
    Task1_mod3 is for Odd Even number and Task2_mod3 is for For loop program addition of 1 to 50.

---
+++ Task1_mod3.py
@@ -0,0 +1,11 @@
+var= input('Enter a number :')
+var=int(var)
+if var % 2 == 0 :
+    print (var, ' is an Even number.')
+else :
+    print (var, ' is an Odd number.')
+
+
+
+#var is a integer value inputted by the user.
+#if else is used for the Odd Even number check.
---
+++ Task2_mod3.py
@@ -0,0 +1,10 @@
+sum=0
+#sum is a variable and initial value with zero.
+sum=int(sum)
+# Sum is a integer type.
+for x in range(1,51) :
+    sum= sum + x
+print('The sum of numbers from 1 to 50 is :', sum)
+
+# for loop used where x iterates between 1 and 50. range function is used for lower and higher count.
+
